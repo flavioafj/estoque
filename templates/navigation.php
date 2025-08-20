b848158e142450538d34ab3c4ce4c5e2d5ae5310
flavioafj <flavioafj@yahoo.com.br>
2025-08-20 07:59:35 -0300

    htaccess

--- templates/navigation.php
+++ templates/navigation.php
@@ -1,7 +1,7 @@
 <?php use Helpers\Session; ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/dashboard.php"><?php echo APP_NAME; ?></a>
+        <a class="navbar-brand" href="dashboard.php"><?php echo APP_NAME; ?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -9,7 +9,7 @@
             <?php if (Session::isLoggedIn()): ?>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/dashboard.php">Dashboard</a>
+                    <a class="nav-link active" aria-current="page" href="dashboard.php">Dashboard</a>
                 </li>
                 </ul>
             <ul class="navbar-nav">
@@ -20,7 +20,7 @@
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarUserDropdown">
                         <li><a class="dropdown-item" href="#">Meu Perfil</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="/logout.php">Sair</a></li>
+                        <li><a class="dropdown-item" href="logout.php">Sair</a></li>
                     </ul>
                 </li>
             </ul>
